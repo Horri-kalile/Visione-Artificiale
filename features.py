b259b7fa461ef07939ea8e89a116d260eb7eb11d
Intissar99 <meddebintissar@gmail.com>
2026-06-20 03:00:08 +0200

    add Hu Moments shape features with Otsu thresholding

--- features.py
+++ features.py
@@ -7,3 +7,10 @@ def extract_color_histogram(image, bins=(8, 8, 8)):
     hist = cv2.calcHist([hsv], [0, 1, 2], None, bins, [0, 180, 0, 256, 0, 256])
     cv2.normalize(hist, hist)
     return hist.flatten()
+
+def extract_hu_moments(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    moments = cv2.moments(thresh)
+    hu_moments = cv2.HuMoments(moments).flatten()
+    return hu_moments
